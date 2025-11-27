88ab74883e6d88a41d2c9268765445b1001e7943
Akash <therealakash13@gmail.com>
2025-11-27 17:49:42 +0530

    window object, hoisting, undefined vs not defined

---
+++ index.js
@@ -0,0 +1,30 @@
+// Execution of javascript program
+// var n = 2;
+// function square(num) {
+//     var ans = num * num;
+//     return ans;
+// }
+
+// var square2 = square(n);
+// var square4 = square(4);
+
+// Window object
+// var x = 10;
+// console.log(window.x); // 10
+// console.log(window.location.href);
+
+// Hoisting
+// console.log(a);   // undefined
+// greet();  // "Hello!"
+
+// var a = 10;
+// function greet() {
+//   console.log("Hello!");
+// }
+
+// undefined vs not defined 
+// var a;
+// console.log(a); // undefined
+// console.log(b);  //undefined
+// var b = 10;
+// console.log(x);  // ReferenceError: x is not defined
